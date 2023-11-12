54ba480a955d0c65b943456432bac531f2f7e99b
jihbiu <wojtowicz.philip@gmail.com>
2023-11-12 23:52:54 +0100

    clean Up

---
+++ 3D-cpp-game/Render/Shader.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <glm/ext/matrix_float4x4.hpp>
+#include <glad/glad.h>
+
+#include "../util/FileUtil.h"
+
+#include <string>
+#include <fstream>
+
+class Shader
+{
+public:
+	Shader() = default;
+	Shader(std::string vertrexPath, std::string fragmentPath);
+	Shader(GLchar vertexShader[], GLchar fragmentShader[]);
+
+	Shader(const Shader&) = delete;
+	Shader& operator=(const Shader&) = delete;
+
+	Shader(Shader&& other) noexcept;
+	Shader& operator=(Shader&& other) noexcept;
+
+	~Shader();
+
+	void use();
+	GLuint getId();
+
+
+	void setInt(const std::string_view &name, int value);
+	void setMat4(const std::string_view name, const glm::mat4& value);
+private:
+	GLuint programId;
+	
+	GLuint createShader(const char* shaderSource, GLenum shaderType);
+	GLuint createProgram(GLuint vertexShader, GLuint fragmentShader, GLuint geometryShader = 0);
+};
+
